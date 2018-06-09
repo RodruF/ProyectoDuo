ddad19e884981ed02deb1c19e0b2063185b21ef7
antonio <antoniojesus.1994@gmail.com>
2018-06-10 00:45:01 +0200

    Otro commit

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -2,14 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { fadeIn } from '../animation';
 
 @Component({
-  selector: 'home',
+  selector: 'app-home',
   templateUrl: './home.component.html',
   animations: [fadeIn]
 })
 export class HomeComponent implements OnInit {
   	title = 'Bienvenido a NGZOO';
 
-	ngOnInit(){
+	ngOnInit() {
 		console.log('home.component cargado !!');
 	}
 }
